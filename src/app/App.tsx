dd6315fe7b48f8064052e8edd78ebeb5555b9a1a
Mateusz Okulewicz <mat@citrusad.com>
2019-06-20 11:19:15 +0200

    introduced google maps

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+
+  public render() {
+    return (
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 37.78825,
+          latitudeDelta: 0.0922,
+          longitude: -122.4324,
+          longitudeDelta: 0.0421,
+        }}
+      />
+    );
+  }
 }
 
 const styles = StyleSheet.create({
