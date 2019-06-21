bb49e3d4efb9f382ee65670d6f5b07674f5cdf70
Mateusz Okulewicz <mat@citrusad.com>
2019-06-21 17:51:19 +0200

    disabled moving map

--- src/app/modules/map/MapContainer.tsx
+++ src/app/modules/map/MapContainer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Text } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Camera, LatLng, Point } from 'react-native-maps';
 import { connect } from 'react-redux';
 import { IStoreState } from '../../store/store';
 import { IUser } from '../../store/types/IUser';
@@ -9,13 +9,33 @@ interface IProps {
   user: IUser;
 }
 
+interface IState {
+  heading: number;
+  cord: LatLng;
+}
+
 function mapStateToProps(state: IStoreState): IProps {
   return {
     user: state.user,
   };
 }
 
-class MapContainer extends React.Component<IProps> {
+class MapContainer extends React.Component<IProps, IState> {
+
+  constructor(props: IProps) {
+    super(props);
+
+
+    this.state = {
+      heading: 0,
+      cord: {
+        latitude: 0,
+        longitude: 0,
+      }
+    };
+
+  }
+
   public render() {
     if (!this.props.user.location) {
       return ( <Text>Loading location...</Text>);
@@ -24,11 +44,22 @@ class MapContainer extends React.Component<IProps> {
     return (
       <MapView
         style={{ flex: 1 }}
-        initialRegion={{
-          latitude: this.props.user.location.latitude,
-          latitudeDelta: 0.0922,
-          longitude: this.props.user.location.longitude,
-          longitudeDelta: 0.0421,
+        minZoomLevel={16}
+        maxZoomLevel={16}
+        scrollEnabled={false}
+        pitchEnabled={false}
+        zoomEnabled={false}
+        showsCompass={false}
+        moveOnMarkerPress={false}
+        camera={{
+          center: {
+            latitude: this.props.user.location.latitude,
+            longitude: this.props.user.location.longitude,
+          },
+          pitch: 45,
+          heading: this.state.heading,
+          altitude: 1000,
+          zoom: 16
         }}
       />
     );
