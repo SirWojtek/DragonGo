1c206d340abb9a92c4d83a4000aa34fba80878c8
Mateusz Okulewicz <mat@citrusad.com>
2019-06-26 14:57:43 +0200

    fixed fetching monsters

--- src/app/store/epics/fetchSpawnAreasEpic.ts
+++ src/app/store/epics/fetchSpawnAreasEpic.ts
@@ -1,26 +1,29 @@
 import {flatten, uniq} from 'lodash';
 import {Epic, ofType} from 'redux-observable';
 import {Action} from 'redux-starter-kit';
+import {concat, of} from 'rxjs';
 import {filter, flatMap, map, tap} from 'rxjs/operators';
 import SpawnAreaService from '../../services/SpawnAreaService';
 import monstersSlice from '../slices/monstersSlice';
 import spawnAreasSlice, {SetSpawnAreasAction} from '../slices/spawnAreasSlice';
 import {SET_LOCATION, SetLocationAction} from '../slices/userSlice';
-import {Actions, IStoreState} from '../store';
+import store, {Actions, IStoreState} from '../store';
 import {ISpawnArea} from '../types/ISpawnArea';
 
 function extractMonsterIds(spawnAreas: ISpawnArea[]): string[] {
-  return uniq(flatten(spawnAreas.map(m => m.id)));
+  return uniq(flatten(spawnAreas.map(a => a.monsters.map(m => m.id))));
 }
 
 const fetchSpawnAreasEpic: Epic<Action, Action, void> = action =>
   action.pipe(
     ofType<Action, SetLocationAction>(SET_LOCATION),
     flatMap(a => SpawnAreaService.getSpawnAreas(a.payload)),
-    tap(spawnAreas =>
-      monstersSlice.actions.fetchMonsters(extractMonsterIds(spawnAreas)),
+    flatMap(spawnAreas =>
+      concat(
+        of(spawnAreasSlice.actions.setSpawnAreas(spawnAreas)),
+        of(monstersSlice.actions.fetchMonsters(extractMonsterIds(spawnAreas))),
+      ),
     ),
-    map(spawnAreas => spawnAreasSlice.actions.setSpawnAreas(spawnAreas)),
   );
 
 export default fetchSpawnAreasEpic;
