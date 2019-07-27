ea427b21f4d9c9ba3c6930fd6e4004bf38618060
Mateusz Okulewicz <mat@citrusad.com>
2019-07-27 15:25:52 +0200

    removed scss

--- src/app/modules/character/views/CharacterNameView.tsx
+++ src/app/modules/character/views/CharacterNameView.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { StyleProp, View, ViewStyle } from 'react-native';
 import { Avatar, Title } from 'react-native-paper';
-import styles from './CharacterNameView.scss';
 
 interface IProps {
   logoUrl?: string;
@@ -14,11 +13,21 @@ class CharacterNameView extends React.Component<IProps> {
       <Avatar.Image size={64} source={{ uri: this.props.logoUrl }} /> :
       <Avatar.Icon size={64} icon="user" />;
 
-    return <View style={styles.container as StyleProp<ViewStyle>}>
+    return <View style={containerStyle}>
       { avatar }
-      <Title style={styles.nameText}>{ this.props.name }</Title>
+      <Title style={nameTextStyle}>{ this.props.name }</Title>
     </View>
   }
 }
+
+const containerStyle: StyleProp<ViewStyle> = {
+  display: 'flex',
+  alignItems: 'center',
+  flexDirection: 'row'
+};
+
+const nameTextStyle: StyleProp<ViewStyle> = {
+  marginLeft: 16,
+};
 
 export default CharacterNameView;
