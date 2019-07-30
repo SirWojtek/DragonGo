1482484c190d638f3c398000e21a1c02129156dd
Mateusz Okulewicz <mat@citrusad.com>
2019-07-30 12:13:35 +0200

    db config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, TypeOrmModule.forRoot()],
 })
 export class AppModule {}
