edbc33c2c083a69e75c050fbd9d8a3c742108e1a
Mateusz Okulewicz <mat@citrusad.com>
2019-08-01 13:02:23 +0200

    repaired import paths

--- backend/src/users/auth/local-strategy.ts
+++ backend/src/users/auth/local-strategy.ts
@@ -2,7 +2,7 @@ import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { UserEntity } from 'src/models/db/user.entity';
+import { UserEntity } from '../../models/db/user.entity';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
