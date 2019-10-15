4a07497f2012b7a7866c93fb6d35a52240ed6cac
Mateusz Okulewicz <mat@citrusad.com>
2019-10-15 10:43:00 +0200

    linter test

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Modal, Portal, Provider as PaperProvider, Text } from 'react-native-paper';
+import { Provider as PaperProvider } from 'react-native-paper';
 import { createAppContainer } from 'react-navigation';
 import { Provider as StoreProvider } from 'react-redux'
 import AppNavigator from './AppNavigator';
 import ModalContainer from './modules/components/ModalContainer';
 import SnackbarContainer from './modules/components/SnackbarContainer';
-import MapContainer from './modules/map/MapContainer';
 import LocationService from './services/LocationService';
 import store from './store/store';
 
