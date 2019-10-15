f92f189bb6c6ae00b4e4072280dc7b42050d80fc
Mateusz Okulewicz <mat@citrusad.com>
2019-10-15 09:48:20 +0200

    logger

--- backend/src/monsters/monsters.gateway.ts
+++ backend/src/monsters/monsters.gateway.ts
@@ -2,16 +2,13 @@ import {
   WebSocketGateway,
   SubscribeMessage,
   WsResponse,
-  WsException,
 } from '@nestjs/websockets';
-import { Client } from 'socket.io';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Monster } from '../models/api/monsters.api';
 import { MonsterInstancesService } from './monster-instances.service';
 import { toMonster } from '../utils/mappers';
 import { UseGuards, UseFilters } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 import { WsJwtGuard } from '../users/auth/ws-jwt.guard';
 import { UserEntity } from '../models/db/user.entity';
 import { HttpToWsExceptionFilter } from '../filters/HttpToWsExceptionFilter';
@@ -24,7 +21,6 @@ export class MonstersGateway {
   @UseGuards(WsJwtGuard)
   @SubscribeMessage('spawn-area-monsters')
   async handleSpawnAreaMonsters(
-    client: Client,
     data: any,
   ): Promise<Observable<WsResponse<Monster[]>>> {
     const user: UserEntity = data.user;
--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, UseGuards, Request, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  UseGuards,
+  Request,
+  Post,
+  Body,
+  Logger,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth/auth.service';
 import { RegisterRequest } from '../models/api/user.api';
@@ -6,6 +13,8 @@ import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  private logger = new Logger(UsersController.name);
+
   constructor(
     private readonly authService: AuthService,
     private readonly usersService: UsersService,
@@ -14,6 +23,7 @@ export class UsersController {
   @UseGuards(AuthGuard('local'))
   @Post('login')
   async login(@Request() req) {
+    this.logger.debug('/users/login');
     return this.authService.login(req.user);
   }
 
