1112a4c1f6fee97d260b83fc9f6a4b6b42527d3e
Mateusz Okulewicz <mat@citrusad.com>
2019-10-27 09:42:15 +0100

    websockets

--- frontend/src/app/services/MonstersService.ts
+++ frontend/src/app/services/MonstersService.ts
@@ -1,11 +1,34 @@
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
+import * as io from 'socket.io-client';
 
-import { IMonster } from '../store/types/IMonster';
-import { monsters } from './data/monsters';
+import { GetSpawnAreaMonsters, Monster } from '../../../../api/monsters.api';
+import { getEnv } from '../../environment/environment';
+
+const env = getEnv();
 
 const MonstersService = {
-  fetchMonsters(ids: string[]): Observable<IMonster[]> {
-    return of(monsters.filter(m => ids.find(id => m.id === id)));
+  watchSpawnAreaMonsters(
+    spawnAreaId: string,
+    accessToken: string
+  ): Observable<Monster[]> {
+    const socket = io.connect(`${env.API_HOST}/monsters`, {
+      extraHeaders: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    } as SocketIOClient.ConnectOpts);
+
+    return new Observable(observer => {
+      socket.on('connect', () => {
+        const request: GetSpawnAreaMonsters = { spawnAreaId };
+        socket.emit('spawn-area-monsters', request);
+
+        socket.on('spawn-area-monsters', (res: Monster[]) =>
+          observer.next(res)
+        );
+        socket.on('exception', (err: Error) => observer.error(err));
+        socket.on('disconnect', () => observer.complete());
+      });
+    });
   }
 };
 
