a0a7529e01ea9aa68a95f615de5b2f98eb7e3007
Mateusz Okulewicz <mat@citrusad.com>
2019-11-24 11:08:17 +0100

    zoom

--- frontend/src/app/modules/map/MapContainer.tsx
+++ frontend/src/app/modules/map/MapContainer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { StyleProp, Text, View, ViewStyle } from 'react-native';
 import MapView, { Point } from 'react-native-maps';
+import { FAB } from 'react-native-paper';
 import { connect } from 'react-redux';
 import { IStoreState } from '../../store/store';
 import { IUser } from '../../store/types/IUser';
@@ -23,7 +24,7 @@ interface IState {
 const PAN_EVENT_TIMESTAMP_MAX_DELTA = 100;
 const PANNING_SPEED = 0.1;
 const ZOOM_OUT = 16;
-const ZOOM_IN = 20;
+const ZOOM_IN = 19;
 
 function mapStateToProps(state: IStoreState): IProps {
   return {
@@ -48,38 +49,54 @@ class MapContainer extends React.Component<IProps, IState> {
     }
 
     return (
-      <MapView
-        style={{ flex: 1 }}
-        customMapStyle={mapStyle}
-        minZoomLevel={this.state.zoom}
-        maxZoomLevel={this.state.zoom}
-        scrollEnabled={false}
-        pitchEnabled={false}
-        zoomEnabled={false}
-        rotateEnabled={false}
-        showsCompass={false}
-        moveOnMarkerPress={false}
-        camera={{
-          center: {
-            latitude: this.props.user.location.lat,
-            longitude: this.props.user.location.lng
-          },
-          pitch: 45,
-          heading: this.state.heading,
-          altitude: 1000,
-          zoom: this.state.zoom
-        }}
-        onPanDrag={event =>
-          this.onPanDrag(event.nativeEvent.position, event.timeStamp)
-        }
-        onDoublePress={() => this.onDoublePress()}
-      >
-        <PlayerMarker
-          coordinate={this.props.user.location}
-          range={this.props.user.maxRange}
-        />
-        <SpawnAreasContainer />
-      </MapView>
+      <View style={{ flex: 1 }}>
+        <MapView
+          style={{ flex: 1 }}
+          customMapStyle={mapStyle}
+          minZoomLevel={this.state.zoom}
+          maxZoomLevel={this.state.zoom}
+          scrollEnabled={false}
+          pitchEnabled={false}
+          zoomEnabled={false}
+          rotateEnabled={false}
+          showsCompass={false}
+          moveOnMarkerPress={false}
+          camera={{
+            center: {
+              latitude: this.props.user.location.lat,
+              longitude: this.props.user.location.lng
+            },
+            pitch: 45,
+            heading: this.state.heading,
+            altitude: 1000,
+            zoom: this.state.zoom
+          }}
+          onPanDrag={event =>
+            this.onPanDrag(event.nativeEvent.position, event.timeStamp)
+          }
+        >
+          <PlayerMarker
+            coordinate={this.props.user.location}
+            range={this.props.user.maxRange}
+          />
+          <SpawnAreasContainer />
+        </MapView>
+        <View style={zoomContainerStyles}>
+          <FAB
+            style={{ marginBottom: 8 }}
+            small={true}
+            icon="add"
+            disabled={this.state.zoom === ZOOM_IN}
+            onPress={() => this.onZoom(1)}
+          />
+          <FAB
+            small={true}
+            icon="remove"
+            disabled={this.state.zoom === ZOOM_OUT}
+            onPress={() => this.onZoom(-1)}
+          />
+        </View>
+      </View>
     );
   }
 
@@ -106,10 +123,17 @@ class MapContainer extends React.Component<IProps, IState> {
     });
   }
 
-  private onDoublePress() {
-    const zoom = this.state.zoom === ZOOM_OUT ? ZOOM_IN : ZOOM_OUT;
+  private onZoom(value: number) {
+    const zoom = this.state.zoom + value;
     this.setState({ zoom });
   }
 }
+
+const zoomContainerStyles: StyleProp<ViewStyle> = {
+  position: 'absolute',
+  margin: 16,
+  right: 0,
+  bottom: 0
+};
 
 export default connect(mapStateToProps)(MapContainer);
